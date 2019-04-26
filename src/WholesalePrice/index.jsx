ed4ce18b9e8d2b81812c232e0d8f4caef1a3533d
bahoss <bahobek@gmail.com>
2019-04-26 19:49:14 +0600

    add popup and header

--- src/WholesalePrice/index.jsx
+++ src/WholesalePrice/index.jsx
@@ -3,11 +3,91 @@ import styled from 'styled-components';
 import PriceList from './priceList';
 import price from './data';
 import SummaryTable from './summaryTable';
+import PopUpForm from './popUp';
+import PopUpFeedback from '../Main/Popup';
+import HeaderImage from './header-image.jpg';
 
 const Wrapper = styled.div`
   background-color: #f5f5f6;
   margin: 0 auto;
   max-width: 967px;
+`;
+
+const HeaderBlock = styled.div`
+  display: flex;
+  height: 150px;
+  margin-top: 30px;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const Image = styled.img`
+  width: 30%;
+`;
+
+const ButtonWrap = styled.div`
+  position: fixed;
+  bottom: 30px;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  background-color: #3b3b3b;
+  color: #fff;
+  cursor: pointer;
+  border: solid 1px #fff;
+  border-radius: 20px;
+  font-family: 'Roboto-Light', sans-serif;
+  font-size: 16px;
+  padding: 10px 25px;
+`;
+
+const HeaderText = styled.div`
+  width: 50%;
+  padding: 10px;
+  & ol {
+    margin: 0;
+    padding: 0 0 0 10px;
+  }
+  & li {
+    margin: 0;
+    padding: 0;
+    font-size: 11px;
+  }
+`;
+
+const Text = styled.h3`
+  margin: 0;
+  font-size: 14px;
+`;
+
+const Span = styled.span`
+  font-size: 12px;
+`;
+
+const Redspan = styled.span`
+  color: red;
+  font-size: 12px;
+  font-weight: bolder;
+`;
+
+const Requisites = styled.div`
+  width: 30%;
+  padding: 10px;
+  border-left: 2px solid #000;
+`;
+
+const RequisitesHeader = styled.h3`
+  font-size: 14px;
+  margin: 0;
+`;
+
+const RequisitesText = styled.h4`
+  padding: 5px;
+  margin: 0;
+  font-size: 10px;
 `;
 
 export default class extends Component {
@@ -16,7 +96,8 @@ export default class extends Component {
     this.state = {
       data: price,
       datasummary: {},
-      summary: 0,
+      popForm: false,
+      popFeedback: false,
     };
   }
 
@@ -46,6 +127,14 @@ export default class extends Component {
         [material]: { ...prevState.datasummary[material], [id]: sum },
       },
     }));
+  };
+
+  popUpOpen = () => {
+    this.setState({ popForm: true });
+  };
+
+  popUpClose = () => {
+    this.setState({ popForm: false });
   };
 
   componentDidMount() {
@@ -91,9 +180,63 @@ export default class extends Component {
   };
 
   render() {
-    const { data, datasummary } = this.state;
+    const {
+      data, datasummary, popForm, popFeedback,
+    } = this.state;
     return (
       <Wrapper>
+        <HeaderBlock>
+          <Image src={HeaderImage} />
+          <HeaderText>
+            <Text>Телефон: +7 (985) 973-07-39</Text>
+            <Text>Почта: info@casplase.ru</Text>
+            <Text>Сайт: https://nagravirovku.ru</Text>
+            <Span>
+              Возможно изготовление донышек
+              {' '}
+              <Redspan>любых форм и размеров </Redspan>
+              {' на заказ '}
+            </Span>
+            <ol>
+              <li>
+                Стоимость указана
+                {' '}
+                <Redspan>при общей закупке товара на сумму от 6300 руб</Redspan>
+              </li>
+              <li>
+                При заказе на сумму более
+                <Redspan>12000</Redspan>
+                {' '}
+                {' рублей доставка транспортной '}
+                компанией до терминала в вашем городе или курьером по городу Москва
+                {' '}
+                <Redspan>Бесплатно.</Redspan>
+              </li>
+              <li>Все заказы из фанеры проходят процесс шлифовки.</li>
+            </ol>
+          </HeaderText>
+          <Requisites>
+            <RequisitesHeader>Реквизиты для заказа:</RequisitesHeader>
+            <RequisitesText>
+              ИП Панченко Андрей Дмитриевич
+              {' '}
+              <br />
+              {'ОГРНИИП 318774600098068 '}
+              <br />
+              {'ИНН 771386120991 '}
+              <br />
+              {'Банк МОСКОВСКИЙ ФИЛИАЛ '}
+              <br />
+              {'АО КБ "МОДУЛЬБАНК"'}
+              <br />
+              БИК 044525092
+              <br />
+              К/с 30101810645250000092
+              <br />
+              {'Счет 40802810870010072151 '}
+            </RequisitesText>
+          </Requisites>
+        </HeaderBlock>
         <PriceList
           data={data}
           handleChangeItemsCount={this.handleChangeItemsCount}
@@ -104,6 +247,11 @@ export default class extends Component {
           datasummary={datasummary}
           calcSummary={this.calcSummary}
         />
+        <ButtonWrap>
+          <Button onClick={this.popUpOpen}>Заказать</Button>
+        </ButtonWrap>
+        <PopUpForm popForm={popForm} closePopForm={this.popUpClose} />
+        <PopUpFeedback isOpened={popFeedback} />
       </Wrapper>
     );
   }
---
+++ src/WholesalePrice/popUp.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import TextInput from '../generic/TextInput';
+import PhoneInput from '../generic/PhoneInput';
+import close from './close.svg';
+import GhostButton from '../generic/GhostButton';
+import validatePhone from '../functions/validatePhone';
+
+const Wrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: ${({ display }) => (display ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  overflow-y: scroll;
+  background: rgba(0, 0, 0, 0.75);
+`;
+
+const CloseButton = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 15px;
+  cursor: pointer;
+`;
+
+const InputName = styled.div`
+  font-size: 14px;
+  color: #4a4a4a;
+  text-align: left;
+  padding: 10px 20px;
+`;
+
+const Label = styled.label`
+  font-size: 16px;
+`;
+
+const ButtonWrapper = styled.div`
+  padding: 20px 0;
+  text-align: center;
+`;
+
+const FormWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  padding: 20px;
+  border-radius: 10px;
+`;
+
+class PopUpForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleChangeForm = (e) => {
+    const state = {};
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  };
+
+  sendOrder = (items, phone, name) => {
+    fetch('/api/order', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ items, name, phone }),
+    })
+      .then(async (data) => {
+        const response = await data.json();
+        this.props.closePopForm();
+      })
+      .catch((/* error */) => {});
+  };
+
+  render() {
+    const { popForm, closePopForm } = this.props;
+    return (
+      <Wrapper display={popForm}>
+        <FormWrapper>
+          <Label>
+            <InputName>Ваше имя</InputName>
+            <TextInput placeholder="Ваше имя" name="name" onChange={this.handleChangeForm} />
+          </Label>
+          <Label>
+            <InputName>Ваш телефон</InputName>
+            <PhoneInput
+              mask="+7 (999) 999-99-99"
+              placeholder="+7"
+              name="phone"
+              onChange={this.handleChangeForm}
+            />
+          </Label>
+          <ButtonWrapper>
+            <GhostButton
+              onClick={(e) => {
+                e.preventDefault();
+                if (this.state.phone && validatePhone(this.state.phone)) {
+                  this.sendOrder(this.state.items, this.state.phone, this.state.name);
+                }
+              }}
+            >
+              Отправить заказ
+            </GhostButton>
+          </ButtonWrapper>
+          <CloseButton src={close} onClick={closePopForm} />
+        </FormWrapper>
+      </Wrapper>
+    );
+  }
+}
+
+export default PopUpForm;
